335f871cca78ca68a7500f4d011473f101b35bf0
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2022-09-22 21:44:13 +0700

    feat: api crud sinh viên

--- index.js
+++ index.js
@@ -1,64 +1,95 @@
-var mysql = require('mysql');
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const port = 3000;
 
-var con = mysql.createConnection({
+app.use(express.json());
+
+const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'studentmanagement'
 });
 
-con.connect(function(err) {
+conn.connect((err) => {
     if (err) throw err;
-    console.log('Connected!');
-    
-    // con.query("CREATE DATABASE stu_db", function(err, result) {
-    //     if (err) throw err;
-    //     console.log('Database created');
-    // });
+    console.log('Connected');
+});
 
-    // con.query("DROP DATABASE stu_db", function(err, result) {
-    //     if (err) throw err;
-    //     console.log('Database drop!');
-    // });
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
-    // var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
-    // con.query(sql, function(err, result) {
-    //     if (err) throw err;
-    //     console.log('Table created');
-    // });
-
-    // var sqlCreate = "CREATE TABLE students (ma VARCHAR(255), hovaten VARCHAR(255), image_url VARCHAR(255), tuoi INT(11), gioitinh INT(1), diachi VARCHAR(255), email VARCHAR(255), phone VARCHAR(255), username VARCHAR(255), password VARCHAR(255))";
-    // con.query(sqlCreate, function(err, result) {
-    //     if (err) throw err;
-    //     console.log('Table created');
-    // });
-
-    // var sqlAlterStudent = "ALTER TABLE students ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
-    // con.query(sqlAlterStudent, function(err, result) {
-    //     if (err) throw err;
-    //     console.log('Table altered');
-    // });
-
-    // var sqlInsert = "INSERT INTO students (hovaten, tuoi) VALUES ('AnhNBT', 27)";
-    // con.query(sqlInsert, function(err, result) {
-    //     if (err) throw err;
-    //     console.log('1 record inserted, ID: ' + result.insertId);
-    // });
-
-    // var sqlInsertMultipleRecords = 'INSERT INTO students (hovaten, tuoi) VALUES ?';
-    // var values = [
-    //     ['Rose', 27],
-    //     ['Khoai Tay', 3],
-    //     ['Tung Lam', 3],
-    //     ['Hong Nguyen', 27]
-    // ];
-    // con.query(sqlInsertMultipleRecords, [values], function(err, result) {
-    //     if (err) throw err;
-    //     console.log('Number of records inserted ' + result.affectedRows);
-    // });
-
-    con.query('SELECT * FROM students', function(err, result) {
+app.get('/api/sinhvien', (req, res) => {
+    console.log(req.query);
+    let sql = 'SELECT * FROM sinhvien';
+    const query = req.query.q;
+    if (query) {
+        sql += ' WHERE LOWER(hovaten) LIKE \'%' + query.toLowerCase() + '%\' OR phone LIKE \'%' + query.toLowerCase() + '%\'';
+    }
+    conn.query(sql, (err, result) => {
         if (err) throw err;
-        console.log(result);
+        res.send(result);
     });
+});
+
+app.get('/api/sinhvien/:id', (req, res) => {
+    const id = req.params.id;
+    conn.query('SELECT * FROM sinhvien WHERE id = ?', [id], (err, result) => {
+        if (err) {
+            res.status(500).send('Internal Server Error ' + err.message);
+        } else {
+            res.status(200).send(result);
+        }
+    });
+});
+
+app.post('/api/sinhvien', (req, res) => {
+    const sinhvien = req.body;
+    conn.query(`INSERT INTO sinhvien (ma, hovaten, email, username, password) VALUES ('${sinhvien.ma}', '${sinhvien.hovaten}', '${sinhvien.email}', '${sinhvien.username}', '${sinhvien.password}')`, (err, result) => {
+        if (err) {
+            res.status(500).send('Internal Server Error ' + err.message);
+        } else {
+            res.status(201).send(sinhvien);
+        }
+    });
+});
+
+app.put('/api/sinhvien/:id', (req, res) => {
+    const id = req.params.id;
+    const sinhvien = req.body;
+    conn.query(`UPDATE sinhvien SET hovaten = '${sinhvien.hovaten}', tuoi = ${sinhvien.tuoi}, gioitinh = ${sinhvien.gioitinh}, diachi = '${sinhvien.diachi}', phone = '${sinhvien.phone}' WHERE id = ${id}`, (err, result) => {
+        if (err) {
+            res.status(500).send('Internal Server Error ' + err.message);
+        } else {
+            console.log(result);
+            if (result.affectedRows > 0) {
+                res.status(200).send('OK');
+            } else {
+                res.status(200).send('Internal Server Error');
+            }
+        }
+    });
+});
+
+app.delete('/api/sinhvien/:id', (req, res) => {
+    const id = req.params.id;
+    console.log('id', id);
+    conn.query('DELETE FROM sinhvien WHERE id = ?', [id], (err, result) => {
+        console.log(result);
+        if (err) {
+            res.status(500).send('Internal Server Error ' + err.message);
+        } else {
+            if (result.affectedRows > 0) {
+                res.status(200).send('OK');
+            } else {
+                res.status(200).send('Internal Server Error');
+            }
+        }
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
